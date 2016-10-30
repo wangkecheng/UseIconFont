e4b50139d41887eb64fc2187d5832fcb3ae89998
wangkecheng <6780894753@qq.com>
2016-10-30 17:15:33 +0800

    使用字体图标

---
+++ LBCommentPagerVC.m
@@ -0,0 +1,11 @@
+使用步骤
+1.先下载 FontLab-Studio 网址  http://soft.macx.cn/soft2042.htm
+  安装需要密码 www.macx.cn
+
+2.然后就是按教程   http://www.jianshu.com/p/115ca5633eef
+
+3.注意事项:  label.text = @"\U0000E802"; 这里的 \U0000 表示unicode 前一部分 然后 E802 这个编码通过 FontLab-Studio打开的字体文件中 需要用的字体图标编号
+
+4.对于文件夹 字体图标文件:里面以.ttf 结尾的就是需要用的字体图标文件
+            字体图标demo: 演示demo
+FontLab-Studio查看字体图标unicode编码工具:  下载好的工具
